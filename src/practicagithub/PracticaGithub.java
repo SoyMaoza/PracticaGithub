baaf3b6b05f8c6c18f616e3b571594ae9bd06f1f
MarioOs <elmausaparalelos@icloud.com>
2023-10-25 22:04:31 -0600

    Cree un println "Segunda Prueba"

--- src/practicagithub/PracticaGithub.java
+++ src/practicagithub/PracticaGithub.java
@@ -6,7 +6,7 @@ public class PracticaGithub {
    
     public static void main(String[] args) {
         System.out.println("Prueba de github");
-        
+        System.out.println("Segunda prueba de github");
     }
     
 }
